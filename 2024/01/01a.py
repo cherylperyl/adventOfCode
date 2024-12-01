79c612bec4bb76afe0b0d78ba755fb67088fe6d9
cherylperyl <cheryl.goh.2020@smu.edu.sg>
2024-12-01 20:17:12 +0800

    2024 Day 1 solutions

--- 2024/01/01a.py
+++ 2024/01/01a.py
@@ -1,5 +1,25 @@
-print("hello world")
+list_one = []
+list_two = []
 
-file = "hello"
+# Open the file in read mode
+with open('input.txt', 'r') as file:
+    # Read line by line
+    for line in file:
+        line_data = line.strip().split("   ")
+        list_one.append(int(line_data[0]))
+        list_two.append(int(line_data[1]))
 
-print(file)
+# Sort list numbers
+list_one.sort()
+list_two.sort()
+
+# Get distances
+total_distance = 0
+for i in range(len(list_one)):
+    list_one_num = list_one[i]
+    list_two_num = list_two[i]
+    distance = abs(list_one_num - list_two_num)
+    total_distance += distance
+
+# Print answer
+print(total_distance)
