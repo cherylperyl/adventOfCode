58904ac67c1bf46ba5a780541d44f8b1c35e8853
cherylperyl <cheryl.goh.2020@smu.edu.sg>
2024-12-03 22:38:08 +0800

    Refactor 2024 Day 3 solutions and add answers

--- year/2024/day03/task1/main.py
+++ year/2024/day03/task1/main.py
@@ -10,43 +10,42 @@ def main():
     corrupted_mem = ""
     for line in sys.stdin:
         corrupted_mem += line.strip("\n")
-    result = 0
-    valid_format = "mul(x,x)" # where x means a 1-3 digit num  
-    num1 = ""
-    num2 = ""
-    is_num = False
-    valid_mul = ""
 
-    
+    result = 0
+    valid_format = "mul(x,x)"
+    num1, num2, valid_mul = "", "", ""
+    is_num = False
+
     for ch in corrupted_mem:
-        if (len(valid_mul) == len(valid_format)) and( len(num1) <= 3) and (len(num2) <= 3):
-            print(valid_format, num1, num2)
+        # Add result of multiplication when a valid mul is found
+        if (valid_mul == valid_format) and (len(num1) <= 3) and \
+                (len(num2) <= 3):
             result += int(num1) * int(num2)
             num1, num2, valid_mul = "", "", ""
 
         # collecting the num
         if ch.isdigit():
-            if len(valid_mul) == 4:
+            if len(valid_mul) in (4, 6):
                 if not is_num:
                     is_num = True
-                num1 += ch
-            elif len(valid_mul) == 6:
-                if not is_num:
-                    is_num = True
-                num2 += ch
+                if len(valid_mul) == 4:
+                    num1 += ch
+                elif len(valid_mul) == 6:
+                    num2 += ch
             continue
         elif is_num and not ch.isdigit():
             valid_mul += "x"
             is_num = False
-    
+
         # checking the rest of the valid format aside from the nums
         if ch != valid_format[len(valid_mul)]:
             num1, num2, valid_mul = "", "", ""
-    
+
         if ch == valid_format[len(valid_mul)]:
             valid_mul += ch
-    
+
     print(result)
+
 
 if __name__ == "__main__":
     main()
--- year/2024/day03/task2/main.py
+++ year/2024/day03/task2/main.py
@@ -12,31 +12,29 @@ def main():
         corrupted_mem += line.strip("\n")
 
     result = 0
-    valid_format = "mul(x,x)" # where x means a 1-3 digit num
-    do = "do()"
-    dont = "don't()"
-    num1 = ""
-    num2 = ""
+    valid_format = "mul(x,x)"
+    num1, num2, valid_mul = "", "", ""
     is_num = False
-    valid_mul = ""
+
+    do, dont = "do()", "don't()"
     enabled = True
     valid_instruction = ""
-    
+
     for ch in corrupted_mem:
-    
+
         if valid_instruction == dont:
             enabled = False
             valid_instruction = ""
         elif valid_instruction == do:
             enabled = True
             valid_instruction = ""
-    
+
         if enabled:
-            if (len(valid_mul) == len(valid_format)) and( len(num1) <= 3) and (len(num2) <= 3):
-                print(valid_format, num1, num2)
+            if (valid_mul == valid_format) and (len(num1) <= 3) and \
+                    (len(num2) <= 3):
                 result += int(num1) * int(num2)
                 num1, num2, valid_mul = "", "", ""
-    
+
             # collecting the num
             if ch.isdigit():
                 if len(valid_mul) == 4:
@@ -51,26 +49,26 @@ def main():
             elif is_num and not ch.isdigit():
                 valid_mul += "x"
                 is_num = False
-    
+
             # checking the rest of the valid format aside from the nums
             if ch != valid_format[len(valid_mul)]:
                 num1, num2, valid_mul = "", "", ""
-    
+
             if ch == valid_format[len(valid_mul)]:
                 valid_mul += ch
-    
+
             # find the next dont()
             if ch == dont[len(valid_instruction)]:
                 valid_instruction += ch
             else:
                 valid_instruction = ""
-    
+
         else:
             if ch == do[len(valid_instruction)]:
                 valid_instruction += ch
             else:
                 valid_instruction = ""
-    
+
     print(result)
 
 
